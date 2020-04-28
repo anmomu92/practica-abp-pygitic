b7ad65f4c84f543c666bd77c18e7e91220ea2533
elssbbboy <elssbbboy@gmail.com>
2020-04-28 04:00:39 +0200

    Added photo selector

--- AccessViewer/src/app/app-routing.module.ts
+++ AccessViewer/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: '**', component: HomeComponent }
+  { path: 'control', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', redirectTo: '/control' }
 ];
 
   //{ path: 'path/:routeParam', component: MyComponent },
--- AccessViewer/src/app/components/home/home.component.ts
+++ AccessViewer/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  uploadForm: FormGroup;
+  picLoaded = false;
+  filePicName = 'Elige archivo';
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.uploadForm = this.formBuilder.group({
+      pictureFile: undefined
+    });
   }
 
+  onSubmitPicture() {
+
+    // this.comm.cloudFunctions('pushPolygon', JSON.parse(this.uploadForm.get('polygonsFile').value)).subscribe(
+    //   (res) => {},
+    //   (err) => console.log(err)
+    // );
+    console.log(this.uploadForm.get('pictureFile').value);
+
+  }
+
+  onPictureSelect(event) {
+    if (event.target.files.length > 0) {
+        const fileReader = new FileReader();
+        this.filePicName = event.target.files[0].name;
+        fileReader.readAsDataURL(event.target.files[0]);
+        fileReader.onload = () => {
+            this.uploadForm.get('pictureFile').setValue(fileReader.result);
+            this.picLoaded = true;
+        };
+        fileReader.onerror = (error) => {
+            console.log(error);
+            this.picLoaded = false;
+        };
+    }
+  }
+  
 }
